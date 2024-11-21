6928fac00774de359d8cf975058119a857bac9fb
Yeswanth Sai Tanneru <78798802+ytanneru@users.noreply.github.com>
2024-11-21 13:52:58 +0530

    Add files via upload

---
+++ Run/Base_Cases_Run_1100_um/generateAndRunTorques.py
@@ -0,0 +1,32 @@
+import math
+import os
+import glob
+import linecache
+
+parameters = [[1100, 0.8, 2000], [1100, 0.8, 2500], [1100, 0.8, 3000],
+	      [1100, 0.7, 2000], [1100, 0.7, 2500], [1100, 0.7, 3000],
+	      [1100, 0.6, 2000], [1100, 0.6, 2500], [1100, 0.6, 3000]]
+
+variables = ['_um_', '_percent_', '_RPM']
+
+# case names
+caseNames = []
+for i in range(len(parameters)):
+	caseNames.append('run_'+str(parameters[i][0])+variables[0]+str(parameters[i][1])+variables[1]+str(parameters[i][2])+variables[2])
+
+home_dir = os.getcwd()
+for i in range(len(caseNames)):
+	cur_dir = home_dir
+	os.chdir(cur_dir)
+	run_dir = cur_dir+'/'+caseNames[i]
+	os.chdir(run_dir)
+	os.system('cp ../Torque_Calculation.py .')
+	
+c_dir = home_dir
+for k in range(len(caseNames)):
+	os.chdir(c_dir)
+	FolderPath = c_dir+'/'+caseNames[k]
+	os.chdir(FolderPath)
+	#print(os.getcwd())
+	print('Running torque calculation in '+caseNames[k]+' ...'+'\n')
+	os.system('python3 Torque_Calculation.py')
---
+++ Run/Base_Cases_Run_1100_um/generateCases.py
@@ -0,0 +1,44 @@
+import math
+import os
+import glob
+import linecache
+
+parameters = [[1100, 0.8, 2000], [1100, 0.8, 2500], [1100, 0.8, 3000],
+	      [1100, 0.7, 2000], [1100, 0.7, 2500], [1100, 0.7, 3000],
+	      [1100, 0.6, 2000], [1100, 0.6, 2500], [1100, 0.6, 3000]]
+
+variables = ['_um_', '_percent_', '_RPM']
+
+# case names (x2) and restart file names for the respective case
+stableCaseNames = []
+caseNames = []
+restartFileNames = []
+for i in range(len(parameters)):
+	stableCaseNames.append('case_'+str(parameters[i][0])+variables[0]+str(parameters[i][1])+variables[1]+str(parameters[i][2])+variables[2])
+	
+	caseNames.append('run_'+str(parameters[i][0])+variables[0]+str(parameters[i][1])+variables[1]+str(parameters[i][2])+variables[2])
+	
+	restartFileNames.append('liggghts.restartCFDEM_'+str(parameters[i][0])+variables[0]+str(parameters[i][1])+variables[1]+str(parameters[i][2])+variables[2])
+
+for k in range(len(caseNames)):
+	c_dir = os.getcwd()
+	MakeCases = os.path.join(c_dir,"MakeCases.sh")
+	MakeCasesRenamed = os.path.join(c_dir,"MakeCases_tmp.sh")
+	os.rename(MakeCases, MakeCasesRenamed)
+
+	with open(MakeCasesRenamed, 'r') as inputFile, open(MakeCases, 'w') as outputFile:
+		for line in inputFile:
+			if 'caseName=' in line:
+				outputFile.write('caseName='+caseNames[k]+'\n')
+			elif 'stableCaseName=' in line:
+				outputFile.write('stableCaseName='+stableCaseNames[k]+'\n')
+			elif 'restartFile=' in line:
+				outputFile.write('restartFile='+restartFileNames[k]+'\n')
+			else:
+				outputFile.write(line)
+	outputFile.close()
+	inputFile.close()
+	os.remove(MakeCasesRenamed)
+	#execute the bashScript
+	os.system('chmod a+x MakeCases.sh')
+	os.system('./MakeCases.sh')
